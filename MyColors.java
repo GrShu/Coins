5319d264d93ec153ac156607fb329bec60f04cb0
GrShu <41187804+GrShu@users.noreply.github.com>
2025-05-28 17:07:17 +0300

    Create MyColors.java

---
+++ MyColors.java
@@ -0,0 +1,11 @@
+package ua.oleksa.coins;
+
+import com.itextpdf.kernel.colors.DeviceRgb;
+
+public class MyColors {
+    public static final DeviceRgb GOLD = new DeviceRgb(255, 215, 0);
+    public static final DeviceRgb SILVER = new DeviceRgb(192, 192, 192);
+    public static final DeviceRgb BRONZE = new DeviceRgb(205, 127, 50);
+    public static final DeviceRgb MYBLUE = new DeviceRgb(76, 135, 186);
+
+}
